5c456b35cd6d89172af5dcd8f62ccd1439d175d4
devadam117 <adam.martinka99@gmail.com>
2022-12-15 19:22:16 +0100

    projekt Done

---
+++ js/priklad.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#priklad').scianimator({
+	    'images': ['images/priklad1.png', 'images/priklad2.png', 'images/priklad3.png', 'images/priklad4.png', 'images/priklad5.png', 'images/priklad6.png', 'images/priklad7.png', 'images/priklad8.png', 'images/priklad9.png', 'images/priklad10.png', 'images/priklad11.png', 'images/priklad12.png', 'images/priklad13.png', 'images/priklad14.png', 'images/priklad15.png', 'images/priklad16.png', 'images/priklad17.png', 'images/priklad18.png', 'images/priklad19.png', 'images/priklad20.png', 'images/priklad21.png', 'images/priklad22.png', 'images/priklad23.png', 'images/priklad24.png', 'images/priklad25.png', 'images/priklad26.png', 'images/priklad27.png', 'images/priklad28.png', 'images/priklad29.png', 'images/priklad30.png', 'images/priklad31.png', 'images/priklad32.png', 'images/priklad33.png', 'images/priklad34.png', 'images/priklad35.png', 'images/priklad36.png', 'images/priklad37.png', 'images/priklad38.png', 'images/priklad39.png', 'images/priklad40.png', 'images/priklad41.png', 'images/priklad42.png', 'images/priklad43.png', 'images/priklad44.png', 'images/priklad45.png', 'images/priklad46.png', 'images/priklad47.png', 'images/priklad48.png', 'images/priklad49.png', 'images/priklad50.png', 'images/priklad51.png'],
+	    'width': 480,
+	    'delay': 250,
+	    'loopMode': 'loop'
+	});
+	$('#priklad').scianimator('play');
+    });
+})(jQuery);
